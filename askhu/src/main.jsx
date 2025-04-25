429391515659269d0e88ef1183c2b77d53cf2bd8
Rishu Kumar <rishukumar9233@gmail.com>
2025-04-25 09:35:26 +0530

    finished UI enhancements

--- askhu/src/main.jsx
+++ askhu/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { VariantsProvider } from './contexts/VariantsContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <VariantsProvider>
+      <App />
+    </VariantsProvider>
   </StrictMode>,
 )
